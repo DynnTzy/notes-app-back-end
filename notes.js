12aa5e27a4ca4e3a42888275cf62d7826da36e3a
Misbahuddin <didin6428@gmail.com>
2024-12-03 02:56:07 +0700

    initial commit

---
+++ notes.js
@@ -0,0 +1,13 @@
+// Array untuk menyimpan catatan
+const notes = [
+    {
+        id: "1",
+        title: "My First Note",
+        createdAt: "2024-11-28T10:00:00Z",
+        updatedAt: "2024-11-28T10:00:00Z",
+        tags: ["example", "note"],
+        body: "This is the content of the note.",
+    },
+];
+
+module.exports = notes;
